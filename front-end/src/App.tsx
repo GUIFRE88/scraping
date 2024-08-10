04ca6dc8d4a1fc411a2281a72e2a2c71d898e9fa
GUIFRE88 <guilherme.fre073@gmail.com>
2024-08-09 22:10:48 -0300

    Create window of perfils

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,26 +1,34 @@
+import { Center, Flex, Square, Text, Box, Heading, Stack } from '@chakra-ui/react';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TableList from './components/table/TableList';
+import ModalInclude from './components/modal/ModalInclude';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Flex
+        direction="column"
+        align="center" 
+        justify="center" 
+        p={4}
+      >
+        <Box w="100%">
+          <Center>
+            <Heading as='h1' size='4xl'>SCRAPING</Heading>
+          </Center>
+          <Flex direction="column" mb={4}>
+            <Box display="flex" justifyContent="flex-end" mb={2}>
+              <ModalInclude />
+            </Box>
+            <Box color='black'>
+              <TableList />
+            </Box>
+          </Flex>
+        </Box>
+      </Flex>
+    </>
+  )
 }
 
 export default App;
---
+++ front-end/src/types/profile.interface.ts
@@ -0,0 +1,11 @@
+export interface Profile {
+    id: number;
+    username: string;
+    followers: number;
+    following: number;
+    stars: number;
+    contributionsLastYear: number;
+    profileImage: string;
+    organization: string;
+    location: string;
+}
